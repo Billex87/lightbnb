5487c1da7543c603292420def53baa89b9ab127d
Billy <billy_mckinnon@hotmail.com>
2021-02-04 01:36:56 +0000

    Added seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (name, email, password)
+VALUES ('Bill McKinnon', 'William@Black.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Paige Lindahl', 'plindahl@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Saad Islam', 'sislam@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (2, 'Happy Place', 'description', 'https://images.pexels.com/photos/2079234/pexels-photo-2079234.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/262488/pexels-photo-262488.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 1000, 2, 4, 5, 'Canada', 'happy street', 'Calgary', 'Alberta', 'T2S 3J6', true),
+(1, 'Dream Place', 'description', 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/262488/pexels-photo-262488.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 2000, 2, 6, 8, 'Canada', 'dream street', 'Calgary', 'Alberta', 'T3S 3K6', true),
+(3, 'Escape Place', 'description', 'https://images.pexels.com/photos/5546512/pexels-photo-5546512.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/262488/pexels-photo-262488.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 3000, 1, 3, 5, 'Canada', 'escape street', 'Calgary', 'Alberta', 'T4S 3M6', true);
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 1, '2021-10-01', '2021-10-14');
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (2, 3, 1, 1, 'Holy Guacamole!'),
+(3, 2, 2, 2, 'I would never stay there again.'),
+(2, 3, 1, 4, 'Can we buy it!? Wow.');
